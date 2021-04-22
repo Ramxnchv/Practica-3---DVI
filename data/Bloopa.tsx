869a7df0f6c23074c75576369ce264bcbb2239d1
pedro <pemaga12@gmail.com>
2021-04-22 16:11:36 +0200

    Mapa medio creado

---
+++ data/Bloopa.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Bloopa" tilewidth="34" tileheight="34" tilecount="2" columns="2">
+ <image source="../images/bloopa.png" width="96" height="48"/>
+</tileset>
